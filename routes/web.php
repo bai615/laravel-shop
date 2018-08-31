176575643ee56e42a4d59c50cd078c55e719d7cb
baihua@sndskj.com <baihua2018>
2018-08-31 18:09:02 +0800

    收货地址列表

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,10 @@ Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
-    Route::get('/email_verification/verify','EmailVerificationController@verify')->name('email_verification.verify');
-    Route::get('/email_verification/send','EmailVerificationController@send')->name('email_verification.send');
+    Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
+    Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
 
     Route::group(['middleware' => 'email_verified'], function () {
-
+        Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
     });
 });

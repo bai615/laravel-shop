80454f50236c6873f6df62e1d9b344f93116cab2
baihua@sndskj.com <baihua2018>
2018-08-31 14:32:10 +0800

    邮箱验证中间件

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,9 +6,15 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    //
+    // 首页
     public function root()
     {
         return view('pages.root');
+    }
+
+    // 邮箱验证通知
+    public function emailVerifyNotice(Request $request)
+    {
+        return view('pages.email_verify_notice');
     }
 }

775c9f7e213cdd5cb4aaeb6b20faf15b56d5b769
baihua@sndskj.com <baihua2018>
2018-09-07 17:21:19 +0800

    商品加入购物车

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -84,9 +84,9 @@
                             });
                     }, function (error) { // 请求失败会执行这个回调
                         // 如果返回码是 401 代表没登录
-                        if(error.response && error.response.status === 401){
+                        if (error.response && error.response.status === 401) {
                             swal('请先登录', '', 'error');
-                        } else if(error.response && error.response.data.msg){
+                        } else if (error.response && error.response.data.msg) {
                             // 其他有 msg 字段的情况，将 msg 提示给用户
                             swal(error.response.data.msg, '', 'error');
                         } else {
@@ -103,6 +103,36 @@
                             .then(function () {
                                 location.reload();
                             });
+                    });
+            });
+
+            // 加入购物车按钮点击事件
+            $('.btn-add-to-cart').click(function () {
+                // 请求加入购物车接口
+                axios.post('{{ route('cart.add') }}', {
+                    sku_id: $('label.active input[name=skus]').val(),
+                    amount: $('.cart_amount input').val(),
+                })
+                    .then(function () { // 请求成功执行此回调
+                        swal('加入购物车成功', '', 'success');
+                    }, function (error) { // 请求失败执行此回调
+                        if (error.response.status === 401) {
+                            // http 状态码为 401 代表用户未登录
+                            swal('请先登录', '', 'error');
+                        } else if (error.response.status === 422) {
+                            // http 状态码为 422 代表用户输入校验失败
+                            var html = '<div>';
+                            _.each(error.response.data.errors, function (errors) {
+                                _.each(errors, function (error) {
+                                    html += error + '<br>';
+                                })
+                            });
+                            html += '</div>';
+                            swal({content: $(html)[0], icon: 'error'});
+                        } else {
+                            // 其他情况应该是系统挂了
+                            swal('系统错误', '', 'error');
+                        }
                     });
             });
         });

2d07bc0ab0c6ed3da3a07ad1ac812ff0031739cf
baihua@sndskj.com <baihua2018>
2018-09-07 16:16:24 +0800

    用户界面 - 收藏商品列表

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@
 
 Route::get('/', 'PagesController@root')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
 Auth::routes();
 
@@ -31,5 +30,8 @@ Route::group(['middleware' => 'auth'], function () {
         Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
         Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
         Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+        Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
     });
 });
+
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
